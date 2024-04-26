b3a108b4e4be9e9c6ea5f6e8f2bc1f29ffd22504
AdifyStudio <161027889+AdifyStudio@users.noreply.github.com>
2024-04-26 23:09:20 +0300

    Update j.js

--- j.js
+++ j.js
@@ -1,7 +1,7 @@
 const musicList = [
     { name: "Silence.mp3", src: "music/Silence.mp3", youtubeLink: "https://www.youtube.com/watch?v=gFouYIoKceA" },
     { name: "Long_Johns.mp3", src: "music/Long_Johns.mp3", youtubeLink: "https://www.youtube.com/watch?v=C9pqe-PD-A0" },
-   
+     { name: "Square_Cooking_Pot.mp3", src: "music/Square_Cooking_Pot.mp3", youtubeLink: "https://www.youtube.com/watch?v=y7NyQ98J4ic&list=PLon15JByMMdESj2WYwh08aTuJSiYDlBAI&index=4" },
     { name: "Before_Death.mp3", src: "music/Before_Death.mp3", youtubeLink: "https://www.youtube.com/watch?v=cVcwG6Lekf8" },
     { name: "Smoky_Mountain.mp3", src: "music/Smoky_Mountain.mp3", youtubeLink: "https://www.youtube.com/watch?v=Jws_ucXYuJ4" },
     { name: "Quivering_Radiance.mp3", src: "music/Quivering_Radiance.mp3", youtubeLink: "https://www.youtube.com/watch?v=7M2OXShodwg" },
